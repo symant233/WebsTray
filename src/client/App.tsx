a6dad87838cb0be98fe4e7abc3c3c0f99324561c
symant233 <symant233@gmail.com>
2024-08-12 17:54:34 +0800

    feat: electron typescript react tailwindcss init

---
+++ src/client/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-zinc-800 grid place-items-center text-7xl text-zinc-300">
+      WebStay
+    </div>
+  );
+}
---
+++ src/client/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './global.css';
+
+const root = createRoot(document.body);
+
+root.render(<App />);
