b612fd7c93db7ef3516e65f5a040daa8c627e7bc
symant233 <symant233@gmail.com>
2024-08-14 09:28:50 +0800

    feat: TrayContent triangle

--- src/client/components/TrayContent.tsx
+++ src/client/components/TrayContent.tsx
@@ -4,8 +4,14 @@ type Props = {
 
 export default function TrayContent({ url }: Props) {
   return (
-    <div className="w-screen h-screen p-2 bg-gray-100">
-      <iframe src={url} className="w-full h-full rounded"></iframe>
+    <div className="w-screen h-screen p-1 pb-3">
+      <div className="w-full h-full bg-gray-100 shadow p-2 pb-8 rounded-lg relative after:content-[''] after:absolute after:-bottom-[12px] after:left-1/2 after:-translate-x-1/2 after:border-x-[12px] after:border-transparent after:border-t-[12px] after:border-t-gray-100">
+        <iframe
+          src={url}
+          className="w-full h-full rounded-lg border border-solid"
+        ></iframe>
+        <div className="select-none pt-1 text-sm text-gray-600">{url}</div>
+      </div>
     </div>
   );
 }
--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,7 @@ const createWindow = () => {
         webPreferences: {
           preload: path.join(__dirname, 'preload.js'),
         },
+        transparent: true,
       },
     };
   });
